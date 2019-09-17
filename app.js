73bf21d6f5f1c66669750c0074440d4d4aceb6c6
Erdinc <Erdinc@gklfmg.com>
2019-09-17 23:53:33 +0200

    finished

--- app.js
+++ app.js
@@ -2,19 +2,16 @@
 
 const express = require('express');
 const bodyParser = require("body-parser");
-const request = require('request');
 
+var nameList = [];
+var todolist = ['Sabah', 'Ogle', 'Ikindi', 'Aksam'];
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// app.get('/', (req, res) => {
-//     res.render('index', { button1: 'Login' });
-// });
 
 app.get('/', (req, res) => {
     res.render('index', { button1: 'Login' });
@@ -24,6 +21,8 @@ app.post('/', (req, res) => {
     let userName = req.body.inputEmail;
     let password = req.body.inputPassword;
 
+    nameList.push(userName);
+
     if (userName === 'admin' && password === 'admin')
         res.redirect('/todolist');
     else
@@ -31,9 +30,9 @@ app.post('/', (req, res) => {
 });
 
 app.get('/todolist', (req, res) => {
-
-    res.render('todoList');
+    res.render('todolist', { name: nameList, toDo: todolist });
 });
+
 
 
 app.listen(5500, () => {
